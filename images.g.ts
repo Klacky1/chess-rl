64b63518d2a6273abee3daa5463a34170c8968a9
Klacky1 <156337996+Klacky1@users.noreply.github.com>
2024-01-24 10:49:58 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,158 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "blackPawn":return img`
+. . . . . . d d d . . . . . . . 
+. . . . . d b b b d . . . . . . 
+. . . . d b b b b b d . . . . . 
+. . . d b b b b b b b d . . . . 
+. . . . d b b b b b d . . . . . 
+. . . . . d b b b d . . . . . . 
+. . . . . d b b b d . . . . . . 
+. . . . . d b b b d . . . . . . 
+. . . . . d b b b d . . . . . . 
+. . . . d b b b b b d . . . . . 
+. . . d b b b b b b b d . . . . 
+. . d b b b b b b b b b d . . . 
+. d b b b b b b b b b b b d . . 
+d b b b b b b b b b b b b b d . 
+d d d d d d d d d d d d d d d . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage0":return img`
+. . . . . . 7 7 7 . . . . . . . 
+. . . . . 7 1 1 1 7 . . . . . . 
+. . . . 7 1 1 1 1 1 . . . . . . 
+. . . 7 1 1 1 1 1 1 1 7 . . . . 
+. . . . 7 1 1 1 1 1 7 . . . . . 
+. . . . . 7 1 1 1 7 . . . . . . 
+. . . . . 7 1 1 1 7 . . . . . . 
+. . . . . 7 1 1 1 7 . . . . . . 
+. . . . . 7 1 1 1 7 . . . . . . 
+. . . . 7 1 1 1 1 1 7 . . . . . 
+. . . 7 1 1 1 1 1 1 1 7 . . . . 
+. . 7 1 1 1 1 1 1 1 1 1 7 . . . 
+. 7 1 1 1 1 1 1 1 1 1 1 1 7 . . 
+7 1 1 1 1 1 1 1 1 1 1 1 1 1 7 . 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "ToweR":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Tower":return img`
+. . f d d f d d f d d f d f . . 
+. . f d d d d d d d d d d f . . 
+. . f d d d d d d d d d d f . . 
+. . f d d d d d d d d d d f . . 
+. . f f f d d d d d d f f f . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . f d d d d d d f . . . . 
+. . . f d d d d d d d d f . . . 
+. . f d d d d d d d d d d f . . 
+. f d d d d d d d d d d d d f . 
+`;
+            case "image5":
+            case "Tower0":return img`
+. . f b b f b b f b b f b f . . 
+. . f b b b b b b b b b b f . . 
+. . f b b b b b b b b b b f . . 
+. . f b b b b b b b b b b f . . 
+. . f f f b b b b b b f f f . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . f b b b b b b b b f . . . 
+. . f b b b b b b b b b b f . . 
+. f b b b b b b b b b b b b f . 
+`;
+            case "image7":
+            case "horse":return img`
+. . . . . d b b b d d . . . . . 
+. . . . d b b b b b d d . . . . 
+. . . d b b b b b b b d d . . . 
+. . d b b b b b b b b b d . . . 
+. . . d b b b b b b b b d . . . 
+. . . . d b b b b b b b d d . . 
+. . . . d b b b b b b b b d . . 
+. . . . d b b b b b b b b d d . 
+. . . . d d b b b b b b b b d . 
+. . . . . d b b b b b b b b d . 
+. . . . . d b b b b b b b b d . 
+. . . . d d b b b b b b b b d . 
+. . . . d b b b b b b b b d d . 
+. . . . d b b b b b b b b d . . 
+. . . . d b b b b b b b b d . . 
+. . . . d d d d d d d d d d . . 
+`;
+            case "image6":
+            case "horse white":return img`
+. . . . . 7 1 1 1 7 7 . . . . . 
+. . . . 7 1 1 1 1 1 7 7 . . . . 
+. . . 7 1 1 1 1 1 1 1 7 7 . . . 
+. . 7 1 1 1 1 1 1 1 1 1 7 . . . 
+. . . 7 1 1 1 1 1 1 1 1 7 . . . 
+. . . . 7 1 1 1 1 1 1 1 7 7 . . 
+. . . . 7 1 1 1 1 1 1 1 1 7 . . 
+. . . . 7 1 1 1 1 1 1 1 1 7 7 . 
+. . . . 7 7 1 1 1 1 1 1 1 1 7 . 
+. . . . . 7 1 1 1 1 1 1 1 1 7 . 
+. . . . . 7 1 1 1 1 1 1 1 1 7 . 
+. . . . 7 7 1 1 1 1 1 1 1 1 7 . 
+. . . . 7 1 1 1 1 1 1 1 1 7 7 . 
+. . . . 7 1 1 1 1 1 1 1 1 7 . . 
+. . . . 7 1 1 1 1 1 1 1 1 7 . . 
+. . . . 7 7 7 7 7 7 7 7 7 7 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0800080002010201020102010102010201020102020102010201020101020102010201020201020102010201010201020102010202010201020102010102010201020102`, img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "transparency8":return transparency8;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile3":
+            case "tile1":return tile1;
+            case "myTile4":
+            case "tile5":return tile5;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
